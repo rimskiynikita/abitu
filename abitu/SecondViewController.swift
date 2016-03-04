7fbbe5c0f026e613003dd471ece4ca962bd66f91
Nikita Rimskiy <rimskiy.nikita@gmail.com>
2016-03-04 22:38:13 +0300

    Initial Commit

---
+++ abitu/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  abitu
+//
+//  Created by Никита Римский on 04.03.16.
+//  Copyright © 2016 Никита Римский. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
